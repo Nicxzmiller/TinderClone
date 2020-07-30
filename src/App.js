0fac6e39ad08e73f6b872c2b9d31879844a9888d
Nicholas <nicxzmiller2@gmail.com>
2020-07-31 00:34:35 +0200

    action buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./Header";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
 
 function App() {
   return (
@@ -15,36 +16,12 @@ function App() {
                 </Route>
                 <Route path="/">
                     <TinderCards/>
+                    <SwipeButtons/>
                 </Route>
             </Switch>
-            {/*Tinder cards*/}
-            {/*Buttons below Tinder cards*/}
-            {/*Chat screen*/}
-            {/*individual chat screen*/}
         </Router>
     </div>
   );
 }
-
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="/__/firebase/7.17.1/firebase-app.js"></script>
-//
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="/__/firebase/7.17.1/firebase-analytics.js"></script>
-//
-// <!-- Initialize Firebase -->
-// <script src="/__/firebase/init.js"></script>
-//
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB6hmnkuKzl3A8JVNYynAA01BzidWZtN0w",
-//   authDomain: "tinder-clone-d4700.firebaseapp.com",
-//   databaseURL: "https://tinder-clone-d4700.firebaseio.com",
-//   projectId: "tinder-clone-d4700",
-//   storageBucket: "tinder-clone-d4700.appspot.com",
-//   messagingSenderId: "971298826995",
-//   appId: "1:971298826995:web:6f320d2cd05058d39da586",
-//   measurementId: "G-1SBY8YD39G"
-// };
 
 export default App;
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,22 +1,20 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import database from "./firebase";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards() {
-    const [people, setPeople ] = useState([
-        {
-            name:'Steve Jobs',
-            url: 'https://www.pioneeringminds.com/wp-content/uploads/2018/10/Steve-Jobs-1440x960.jpg'
-        },
-        {
-            name:'Tom Ford',
-            url: 'https://makerfairerome.eu/wp-content/uploads/2020/04/tomford01-1027x560.jpg'
-        }
-    ]);
+    const [people, setPeople ] = useState([]);
+
+    //useEffect is a piece of code that runs based on a condition
+    useEffect(() => {
+        database.collection('people').onSnapshot(snapshot => (
+            setPeople(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, []);
 
     return(
         <div>
-            <h1>Tinder Cards</h1>
             <div className="tinderCards__cardContainer">
                 {people.map(person => (
                     <TinderCard className="swipe" key={person.name} preventSwipe={["up", "down"]}>
